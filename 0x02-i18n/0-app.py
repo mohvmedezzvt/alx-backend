c5c9d8c82d306b1a9736ffcc87c5aa36160a544e
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-04-09 14:17:08 +0200

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    """ Main route
+
+    Returns:
+        str: template
+    """
+    return render_template('0-index.html')
